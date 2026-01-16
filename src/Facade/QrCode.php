c7c390d838d50b1da987de6ae7bc335cb690e4d2
lede <cighsen02@163.com>
2026-01-16 23:51:15 +0800

    feat(qrcode): implement QR code generator for Laravel

--- src/Facade/QrCode.php
+++ src/Facade/QrCode.php
@@ -16,6 +16,18 @@ namespace Twoir\QrCode\Facade;
 use Illuminate\Support\Facades\Facade;
 use Twoir\QrCode\Services\QrCode as QrCodeService;
 
+/**
+ * @method static \Twoir\QrCode\Services\QrCode setOutputBase64(bool $enable)
+ * @method static \Twoir\QrCode\Services\QrCode isOutputBase64()
+ * @method static \Twoir\QrCode\Services\QrCode getMimeType()
+ * @method static \chillerlan\QRCode\QRCode render(?string $data = null, ?string $file = null)
+ * @method static \chillerlan\QRCode\QRCode setOptions(\chillerlan\Settings\SettingsContainerInterface $options)
+ * @method static \chillerlan\QRCode\QRCode readFromBlob(string $blob)
+ * @method static \chillerlan\QRCode\QRCode readFromFile(string $path)
+ * @method static \chillerlan\QRCode\QRCode readFromSource(\chillerlan\QRCode\Common\LuminanceSourceInterface $source)
+ *
+ * @see \Twoir\QrCode\Services\QrCode
+ */
 class QrCode extends Facade
 {
     protected static function getFacadeAccessor()
